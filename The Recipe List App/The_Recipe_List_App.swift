ff97c6f0d4fc5db06d791f765f73ddccc325d3ff
Forjam30 <james.formosa30@gmail.com>
2022-01-25 17:49:11 +0000

    Entry Point changed to TabView

--- The Recipe List App/The_Recipe_List_App.swift
+++ The Recipe List App/The_Recipe_List_App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct The_Recipe_List_App: App {
     var body: some Scene {
         WindowGroup {
-            RecipeListView()
+            RecipeTabView()
         }
     }
 }
---
+++ The Recipe List App/View/RecipeTabView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeTabView.swift
+//  The Recipe List App
+//
+//  Created by James Formosa on 25/01/2022.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
