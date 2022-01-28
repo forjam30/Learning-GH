9bbf47da3d813a2dea5cd835bed4679fc975bfa6
Forjam30 <james.formosa30@gmail.com>
2022-01-28 18:40:56 +0000

    Added featured view

--- The Recipe List App/View/RecipeFeaturedView.swift
+++ The Recipe List App/View/RecipeFeaturedView.swift
@@ -10,12 +10,57 @@ import SwiftUI
 struct RecipeFeaturedView: View {
     @EnvironmentObject var model: RecipeModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        
+        VStack(spacing:0){
+            Text("Featured Recipes").font(.largeTitle).fontWeight(.bold).padding(.top, 50.0)
+        GeometryReader{ geo in
+        TabView{
+            ForEach(0..<model.recipes.count){ index in
+            
+                if model.recipes[index].featured == true {
+                    
+                    VStack{
+            ZStack{
+            Rectangle().foregroundColor(.white)
+                VStack{
+                Image(model.recipes[index].image)
+                    .resizable()
+                    .aspectRatio( contentMode: .fill)
+                    .clipped()
+                   
+                    Text(model.recipes[index].name).padding().font(.title)
+                    VStack(alignment: .leading){
+                        Text("Prep Time: ")
+                            .font(.headline)
+                            .padding(.bottom, 5.0)
+                            
+                        Text(model.recipes[index].prepTime).padding(.bottom, 5.0)
+                    Text("Fucking tasty shit")
+                    }
+                    .padding(.trailing, 200.0)
+                }
+            
+            
+            }.frame(width: geo.size.width * 9 / 10, height: geo.size.height * 3 / 4, alignment: .center).shadow(radius: 15).cornerRadius(10)
+                   
+            }
+                }
+            }
+        }
+        .padding(.top, -100.0)
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+    }
+        }
+        
     }
 }
 
+
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeFeaturedView()
+        RecipeFeaturedView().environmentObject(RecipeModel())
     }
 }
